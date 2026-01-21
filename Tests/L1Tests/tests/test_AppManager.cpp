f01d534aa6b0452a2ad3347d8606109b698caa2b
ABIRAMI-S <abiramissn@gmail.com>
2026-01-21 16:44:55 +0530

    Update test_AppManager.cpp

--- Tests/L1Tests/tests/test_AppManager.cpp
+++ Tests/L1Tests/tests/test_AppManager.cpp
@@ -40,8 +40,6 @@
 
 
 #define TEST_LOG(x, ...) fprintf(stderr, "\033[1;32m[%s:%d](%s)<PID:%d><TID:%d>" x "\n\033[0m", __FILE__, __LINE__, __FUNCTION__, getpid(), gettid(), ##__VA_ARGS__); fflush(stderr);
-#define DEBUG_PRINTF(fmt, ...) \
-    std::printf("[DEBUG] %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__)
 
 #define TIMEOUT   (80000)
 #define APPMANAGER_APP_ID           "com.test.app"
