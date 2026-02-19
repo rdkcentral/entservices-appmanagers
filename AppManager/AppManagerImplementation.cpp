6864c7e150a98f2d6c9f2f51dd5bed047a4993ad
Madana Gopal <madanagopal123@gmail.com>
2026-02-19 16:07:01 +0530

    Add way to prevent unlock called twice

--- AppManager/AppManagerImplementation.cpp
+++ AppManager/AppManagerImplementation.cpp
@@ -269,13 +269,21 @@ void AppManagerImplementation::Dispatch(EventNames event, const JsonObject param
                 {
                     notification->OnAppLifecycleStateChanged(appId, appInstanceId, newState, oldState, errorReason);
                 }
-                mAdminLock.Unlock();
                 if ((Exchange::IAppManager::AppLifecycleState::APP_STATE_LOADING == oldState) && (Exchange::IAppManager::AppLifecycleState::APP_STATE_LOADING == newState))
                 {
                     LOGERR("Transition from loading state failed. Killing the application ....");
-                    Core::hresult status = KillApp(appId);
+                    Core::hresult status = Core::ERROR_UNAVAILABLE;
+                    if (mLifecycleInterfaceConnector != nullptr)
+                    {
+                        status = mLifecycleInterfaceConnector->killApp(appId);
+                    }
+                    else
+                    {
+                        LOGERR("mLifecycleInterfaceConnector is null, cannot kill app %s", appId.c_str());
+                    }
                     LOGERR("Kill app status in loading state [%d]....", static_cast<int>(status));
                 }
+                mAdminLock.Unlock();
             }
             break;
         }
