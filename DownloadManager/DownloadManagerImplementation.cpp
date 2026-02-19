daf183298393fcb42a4fe203d0eedf002dfa8d44
Suresh Kumar <suresh_kumar3@comcast.com>
2026-02-19 10:19:48 -0800

    cleanup

--- DownloadManager/DownloadManagerImplementation.cpp
+++ DownloadManager/DownloadManagerImplementation.cpp
@@ -18,7 +18,6 @@
 **/
 
 #include <chrono>
-#include <cinttypes>
 
 #include "DownloadManagerImplementation.h"
 
@@ -491,7 +490,7 @@ namespace Plugin {
                 long httpCode = mHttpClient->getStatusCode();
                 if (status == DownloadManagerHttpClient::Status::Success)
                 {
-                    LOGINFO("DM: Download succeeded (took %" PRIu64 " ms): id=%s url=%s file=%s retries=%d rateLimit=%u http_code=%ld",
+                    LOGINFO("DM: Download succeeded (took %lldms): id=%s url=%s file=%s retries=%d rateLimit=%u http_code=%ld",
                     elapsed, downloadRequest->getId().c_str(), downloadRequest->getUrl().c_str(),
                     downloadRequest->getFileLocator().c_str(), downloadRequest->getRetries(),
                     downloadRequest->getRateLimit(), httpCode);
@@ -506,7 +505,7 @@ namespace Plugin {
                     break;
                 }
 
-                LOGDBG("DM: Attempt download (%d/%d): status=%d http_code=%ld elapsed=%" PRIu64 " ms",
+                LOGDBG("DM: Attempt download (%d/%d): status=%d http_code=%ld elapsed=%lld ms",
                     attemptCount, downloadRequest->getRetries(), status, httpCode, elapsed);
             }
 
