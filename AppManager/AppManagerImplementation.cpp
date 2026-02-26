eeee0fe208ef297c1f42916f0db8d5617a812f38
mounikapalem <mounikan163@gmail.com>
2026-02-26 07:32:40 +0000

    RDKEMW-14321: Unlock method is called twice for a successful app launch
        Reason for change: Resolved issue where the unlock method was invoked twice

--- AppManager/AppManagerImplementation.cpp
+++ AppManager/AppManagerImplementation.cpp
@@ -1034,10 +1034,6 @@ Core::hresult AppManagerImplementation::TerminateApp(const string& appId )
         if (nullptr != mLifecycleInterfaceConnector)
         {
             status = mLifecycleInterfaceConnector->terminateApp(appId);
-            if(status == Core::ERROR_NONE)
-            {
-                status = packageUnLock(appId);
-            }
         }
 #ifdef ENABLE_AIMANAGERS_TELEMETRY_METRICS
         if(status == Core::ERROR_NONE)
