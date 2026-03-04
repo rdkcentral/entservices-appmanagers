6d7c3653a4a17c97c4607ab8b0134afa33dc52fb
db982 <db982@apac.comcast.com>
2026-03-04 11:17:32 +0530

    cleanup the l2tests.cmake and l1tests.yml

--- Tests/testframework/l2tests.cmake
+++ Tests/testframework/l2tests.cmake
@@ -24,77 +24,31 @@ file(GLOB BASEDIR Tests)
 set(BASEDIR ${BASEDIR}/headers)
 set(EMPTY_HEADERS_DIRS
         ${BASEDIR}
-        ${BASEDIR}/rdk/ds
         ${BASEDIR}/rdk/iarmbus
         ${BASEDIR}/rdk/iarmmgrs-hal
-        ${BASEDIR}/systemservices
-        ${BASEDIR}/systemservices/proc
+        ${BASEDIR}/libusb
         )
 
 set(EMPTY_HEADERS
-         ${BASEDIR}/rdk/ds/host.hpp
-         ${BASEDIR}/rdk/ds/videoOutputPort.hpp
-         ${BASEDIR}/rdk/ds/audioOutputPort.hpp
-         ${BASEDIR}/rdk/ds/dsMgr.h
-         ${BASEDIR}/rdk/ds/manager.hpp
-         ${BASEDIR}/rdk/ds/audioOutputPortType.hpp
-         ${BASEDIR}/rdk/ds/audioOutputPortConfig.hpp
-         ${BASEDIR}/rdk/ds/pixelResolution.hpp
          ${BASEDIR}/rdk/iarmbus/libIARM.h
          ${BASEDIR}/rdk/iarmbus/libIBus.h
          ${BASEDIR}/rdk/iarmbus/libIBusDaemon.h
-         ${BASEDIR}/rdk/iarmmgrs-hal/pwrMgr.h
          ${BASEDIR}/rdk/iarmmgrs-hal/mfrMgr.h
          ${BASEDIR}/rdk/iarmmgrs-hal/sysMgr.h
-         ${BASEDIR}/rdk_logger_milestone.h
-         ${BASEDIR}/rdk/ds/videoOutputPortConfig.hpp
-         ${BASEDIR}/rdk/ds/sleepMode.hpp
-         ${BASEDIR}/rdk/ds/frontPanelConfig.hpp
-         ${BASEDIR}/rdk/ds/frontPanelTextDisplay.hpp
          ${BASEDIR}/rfcapi.h
          ${BASEDIR}/rbus.h
-         ${BASEDIR}/systemservices/proc/readproc.h
-         ${BASEDIR}/systemservices/secure_wrapper.h
          ${BASEDIR}/libudev.h
-         ${BASEDIR}/systemaudioplatform.h
-         ${BASEDIR}/rdk/ds/dsTypes.h
-         ${BASEDIR}/rdk/ds/hdmiIn.hpp
-         ${BASEDIR}/rdk/ds/compositeIn.hpp
-         ${BASEDIR}/rdk/ds/exception.hpp
-         ${BASEDIR}/rdk/iarmbus/iarmUtil.h
-         ${BASEDIR}/rdk/ds/dsError.h
-         ${BASEDIR}/tvTypes.h
-         ${BASEDIR}/tvSettingsExtODM.h 
-         ${BASEDIR}/tvError.h
-         ${BASEDIR}/tvSettingsODM.h
-         ${BASEDIR}/tr181api.h
-         ${BASEDIR}/dsRpc.h
+         ${BASEDIR}/libusb/libusb.h
+         ${BASEDIR}/secure_wrapper.h
+         ${BASEDIR}/wpa_ctrl.h
+         ${BASEDIR}/maintenanceMGR.h
+         ${BASEDIR}/pkg.h
          ${BASEDIR}/edid-parser.hpp
-         ${BASEDIR}/videoOutputPortType.hpp
-         ${BASEDIR}/videoResolution.hpp
-         ${BASEDIR}/audioOutputPortType.hpp
-         ${BASEDIR}/audioOutputPortConfig.hpp
-         ${BASEDIR}/dsUtl.h
-         ${BASEDIR}/dsError.h
-         ${BASEDIR}/list.hpp
-         ${BASEDIR}/dsDisplay.h
          ${BASEDIR}/Wraps.h
-         ${BASEDIR}/rdk/ds/AudioStereoMode.hpp
-         ${BASEDIR}/rdk/ds/VideoDFC.hpp
+         ${BASEDIR}/telemetry_busmessage_sender.h
+         ${BASEDIR}/gdialservice.h
+         ${BASEDIR}/gdialservicecommon.h
          )
-     if(RDK_SERVICE_CPC_L2_TEST)
-         set(EMPTY_HEADERS
-             ${BASEDIR}/sec_security_comcastids.h
-             ${BASEDIR}/KeyProvisionClient.h
-             ${BASEDIR}/KeyProvisionResult.h
-             ${BASEDIR}/KeyProvisionObject.h
-             ${BASEDIR}/KeyProvisionDirectorySerializer.h
-             ${BASEDIR}/SecApiProvisioner.h
-             ${BASEDIR}/CredentialUtils.h
-             ${BASEDIR}/fkpsb.h
-             ${BASEDIR}/sec_security.h
-             )
-     endif()
 
 
 file(MAKE_DIRECTORY ${EMPTY_HEADERS_DIRS})
@@ -114,35 +68,20 @@ message("Adding compiler and linker options for all targets")
 
 file(GLOB BASEDIR Tests/mocks)
 set(FAKE_HEADERS
-        ${BASEDIR}/devicesettings.h
         ${BASEDIR}/Iarm.h
         ${BASEDIR}/Rfc.h
         ${BASEDIR}/RBus.h
+        ${BASEDIR}/Telemetry.h
         ${BASEDIR}/Udev.h
         ${BASEDIR}/libusb/libusb.h
         ${BASEDIR}/secure_wrappermock.h
+        ${BASEDIR}/wpa_ctrl_mock.h
         ${BASEDIR}/readprocMockInterface.h
-        ${BASEDIR}/btmgr.h
-        ${BASEDIR}/tvSettings.h
-        ${BASEDIR}/tr181api.h
+        ${BASEDIR}/maintenanceMGR.h
+        ${BASEDIR}/pkg.h
+        ${BASEDIR}/gdialservice.h
         ${BASEDIR}/Wraps.h
-	${BASEDIR}/essos-resmgr.h
-	${BASEDIR}/rdk_logger_milestone.h
-	${BASEDIR}/dsFPD.h
         )
-    if(RDK_SERVICE_CPC_L2_TEST)
-        set(FAKE_HEADERS
-            ${BASEDIR}/sec_security_comcastids.h
-            ${BASEDIR}/KeyProvisionClient.h
-            ${BASEDIR}/KeyProvisionResult.h
-            ${BASEDIR}/KeyProvisionObject.h
-            ${BASEDIR}/KeyProvisionDirectorySerializer.h
-            ${BASEDIR}/SecApiProvisioner.h
-            ${BASEDIR}/CredentialUtils.h
-            ${BASEDIR}/fkpsb.h
-            ${BASEDIR}/sec_security.h
-            )
-    endif()
 
 
 foreach (file ${FAKE_HEADERS})
@@ -150,32 +89,24 @@ foreach (file ${FAKE_HEADERS})
 endforeach ()
 
 add_compile_options(-Wall -Werror)
-add_link_options(-Wl,-wrap,setmntent -Wl,-wrap,getmntent -Wl,-wrap,v_secure_popen -Wl,-wrap,v_secure_pclose -Wl,-wrap,v_secure_system -Wl,-wrap,readlink)
+add_link_options(-Wl,-wrap,system -Wl,-wrap,popen -Wl,-wrap,syslog -Wl,-wrap,v_secure_popen -Wl,-wrap,v_secure_pclose -Wl,-wrap,v_secure_system -Wl,-wrap,pclose)
 
 add_definitions(
          -DUSE_IARMBUS
          -DRFC_ENABLED
-         -DUSE_DS
-         -DUSE_DRM_SCREENCAPTURE
-         -DENABLE_THERMAL_PROTECTION
          -DHAS_API_SYSTEM
-         -DHAS_API_POWERSTATE
          -DHAS_RBUS
-	 -DENABLE_ERM
          -DDISABLE_SECURITY_TOKEN
-         -DBUILD_ENABLE_NOT_ACTIVATED
-         -DAUTHTOKEN_FETCH_BINARY="/opt/www/authService/authservice"         
+         -DENABLE_TELEMETRY_LOGGING
+         -DENABLE_DEVICE_MANUFACTURER_INFO
          -DUNIT_TESTING
 )
 
 message("Setting build options")
-set(CMAKE_DISABLE_FIND_PACKAGE_DS ON)
 set(CMAKE_DISABLE_FIND_PACKAGE_IARMBus ON)
 set(CMAKE_DISABLE_FIND_PACKAGE_Udev ON)
 set(CMAKE_DISABLE_FIND_PACKAGE_RFC ON)
 set(CMAKE_DISABLE_FIND_PACKAGE_RBus ON)
-set(CMAKE_DISABLE_FIND_PACKAGE_CEC ON)
-set(CMAKE_DISABLE_FIND_PACKAGE_Dobby ON)
-set(CMAKE_DISABLE_FIND_PACKAGE_CEC ON)
 set(BUILD_SHARED_LIBS ON)
-set(DS_FOUND ON)
+set(LIBOPKG_LIBRARIES ${LIBOPKG_LIBRARIES} CACHE PATH "Path to LIBOPKG library")
+set(LIBOPKG_INCLUDE_DIRS ${LIBOPKG_INCLUDE_DIRS} CACHE PATH "Path to LIBOPKG include")
