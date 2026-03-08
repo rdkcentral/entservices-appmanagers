f12d81bd2673457fd0d695a31ba396e87bbca128
db982 <db982@apac.comcast.com>
2026-03-08 21:35:25 +0530

    RDKServicesl1test testing

---
+++ Tests/L1Tests/runner.cpp
@@ -0,0 +1,29 @@
+/**
+ * If not stated otherwise in this file or this component's LICENSE
+ * file the following copyright and licenses apply:
+ *
+ * Copyright 2024 RDK Management
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+/**
+ * @file runner.cpp
+ * @brief Entry point for the RdkServicesL1Test binary.
+ *
+ * This file is intentionally empty of test code.
+ * The main() function is provided by GTest::gmock_main (linked via CMakeLists.txt).
+ * All test cases are registered by the plugin test shared libraries linked to this binary
+ * (e.g. libWPEFrameworkL1TestsIN.so), which are discovered automatically by the
+ * GTest runtime.
+ */
