70059a7f83bc59da2c6dd7dced999fb3de648134
sajilal711 <shreyas.sajilal@gmail.com>
2026-03-18 14:28:52 +0530

    remove flagged mocks

--- Tests/mocks/RdkWindowManagerMock.h
+++ Tests/mocks/RdkWindowManagerMock.h
@@ -34,6 +34,9 @@ public:
     CompositorControllerImplMock() = default;
     virtual ~CompositorControllerImplMock() = default;
 
+    using ListenerPropertyMap = std::map<std::string, RdkWindowManagerData>;
+    using EventPayload = std::vector<std::map<std::string, RdkWindowManagerData>>;
+
     MOCK_METHOD(void, initialize, (), (override));
     MOCK_METHOD(bool, moveToFront, (const std::string& client), (override));
     MOCK_METHOD(bool, moveToBack, (const std::string& client), (override));
@@ -43,28 +46,73 @@ public:
     MOCK_METHOD(bool, kill, (const std::string& client), (override));
     MOCK_METHOD(bool, addKeyIntercept, (const std::string& client, const uint32_t& keyCode, const uint32_t& flags, const bool& focusOnly, const bool& propagate), (override));
     MOCK_METHOD(bool, removeKeyIntercept, (const std::string& client, const uint32_t& keyCode, const uint32_t& flags), (override));
-    MOCK_METHOD(bool, addKeyListener, (const std::string& client, const uint32_t& keyCode, const uint32_t& flags, (std::map<std::string, RdkWindowManagerData>&) listenerProperties), (override));
-    MOCK_METHOD(bool, addNativeKeyListener, (const std::string& client, const uint32_t& keyCode, const uint32_t& flags, (std::map<std::string, RdkWindowManagerData>&) listenerProperties), (override));
+    MOCK_METHOD(
+        bool,
+        addKeyListener,
+        (const std::string& client,
+         const uint32_t& keyCode,
+         const uint32_t& flags,
+         ListenerPropertyMap& listenerProperties),
+        (override));
+    MOCK_METHOD(
+        bool,
+        addNativeKeyListener,
+        (const std::string& client,
+         const uint32_t& keyCode,
+         const uint32_t& flags,
+         ListenerPropertyMap& listenerProperties),
+        (override));
     MOCK_METHOD(bool, removeKeyListener, (const std::string& client, const uint32_t& keyCode, const uint32_t& flags), (override));
     MOCK_METHOD(bool, removeAllKeyListeners, (), (override));
     MOCK_METHOD(bool, removeAllKeyIntercepts, (), (override));
     MOCK_METHOD(bool, removeNativeKeyListener, (const std::string& client, const uint32_t& keyCode, const uint32_t& flags), (override));
     MOCK_METHOD(bool, injectKey, (const uint32_t& keyCode, const uint32_t& flags), (override));
-    MOCK_METHOD(bool, generateKey, (const std::string& client, const uint32_t& keyCode, const uint32_t& flags, std::string virtualKey), (override));
-    MOCK_METHOD(bool, generateKey, (const std::string& client, const uint32_t& keyCode, const uint32_t& flags, std::string virtualKey, double duration), (override));
-    MOCK_METHOD(bool, getScreenResolution, (uint32_t& width, uint32_t& height), (override));
-    MOCK_METHOD(bool, setScreenResolution, (uint32_t width, uint32_t height), (override));
-    MOCK_METHOD(bool, getClients, (std::vector<std::string>& clients), (override));
-    MOCK_METHOD(bool, getZOrder, (const std::string& client, int32_t& zorder), (override));
-    MOCK_METHOD(bool, setZorder, (const std::string& client, int32_t zorder), (override));
-    MOCK_METHOD(bool, getBounds, (const std::string& client, uint32_t& x, uint32_t& y, uint32_t& width, uint32_t& height), (override));
-    MOCK_METHOD(bool, setBounds, (const std::string& client, uint32_t x, uint32_t y, uint32_t width, uint32_t height), (override));
+    using ClientList = std::vector<std::string>;
+
+    MOCK_METHOD(bool, getClients, (ClientList& clients), (override));
     MOCK_METHOD(bool, getVisibility, (const std::string& client, bool& visible), (override));
     MOCK_METHOD(bool, setVisibility, (const std::string& client, bool visible), (override));
+
+    MOCK_METHOD(
+        bool,
+        getScreenResolution,
+        (uint32_t& width, uint32_t& height),
+        (override));
+    MOCK_METHOD(
+        bool,
+        setScreenResolution,
+        (uint32_t width, uint32_t height),
+        (override));
+
+    MOCK_METHOD(bool, getZOrder, (const std::string& client, int32_t& zorder), (override));
+    MOCK_METHOD(bool, setZorder, (const std::string& client, int32_t zorder), (override));
+
     MOCK_METHOD(bool, getOpacity, (const std::string& client, unsigned int& opacity), (override));
     MOCK_METHOD(bool, setOpacity, (const std::string& client, unsigned int opacity), (override));
     MOCK_METHOD(bool, getScale, (const std::string& client, double& scaleX, double& scaleY), (override));
     MOCK_METHOD(bool, setScale, (const std::string& client, double scaleX, double scaleY), (override));
+
+    MOCK_METHOD(
+        bool,
+        getBounds,
+        (const std::string& client, uint32_t& x, uint32_t& y, uint32_t& width, uint32_t& height),
+        (override));
+    MOCK_METHOD(
+        bool,
+        setBounds,
+        (const std::string& client, uint32_t x, uint32_t y, uint32_t width, uint32_t height),
+        (override));
+
+    MOCK_METHOD(
+        bool,
+        generateKey,
+        (const std::string& client, const uint32_t& keyCode, const uint32_t& flags, std::string virtualKey),
+        (override));
+    MOCK_METHOD(
+        bool,
+        generateKey,
+        (const std::string& client, const uint32_t& keyCode, const uint32_t& flags, std::string virtualKey, double duration),
+        (override));
     MOCK_METHOD(bool, getHolePunch, (const std::string& client, bool& holePunch), (override));
     MOCK_METHOD(bool, setHolePunch, (const std::string& client, bool holePunch), (override));
     MOCK_METHOD(bool, getCrop, (const std::string& client, int32_t& cropX, int32_t& cropY, int32_t& cropWidth, int32_t& cropHeight), (override));
@@ -93,7 +141,7 @@ public:
     MOCK_METHOD(bool, getLogLevel, (std::string& level), (override));
     MOCK_METHOD(bool, setTopmost, (const std::string& client, bool topmost, bool focus), (override));
     MOCK_METHOD(bool, getTopmost, (std::string& client), (override));
-    MOCK_METHOD(bool, sendEvent, (const std::string& eventName, (std::vector<std::map<std::string, RdkWindowManagerData>>&) data), (override));
+    MOCK_METHOD(bool, sendEvent, (const std::string& eventName, EventPayload& data), (override));
     MOCK_METHOD(bool, enableKeyRepeats, (bool enable), (override));
     MOCK_METHOD(bool, getKeyRepeatsEnabled, (bool& enable), (override));
     MOCK_METHOD(bool, getVirtualResolution, (const std::string& client, uint32_t& virtualWidth, uint32_t& virtualHeight), (override));
