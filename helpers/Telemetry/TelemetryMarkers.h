0f90ef2e412d0a73db4cd1847685b672d67a8bc4
sajilal711 <shreyas.sajilal@gmail.com>
2026-03-20 12:07:16 +0530

    update markers with prefix

--- helpers/Telemetry/TelemetryMarkers.h
+++ helpers/Telemetry/TelemetryMarkers.h
@@ -1,6 +1,6 @@
-/*
-* If not stated otherwise in this file or this component's LICENSE file the
-* following copyright and licenses apply:
+/**
+* If not stated otherwise in this file or this component's LICENSE
+* file the following copyright and licenses apply:
 *
 * Copyright 2026 RDK Management
 *
@@ -15,22 +15,24 @@
 * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 * See the License for the specific language governing permissions and
 * limitations under the License.
-*/
+**/
 
 #pragma once
 
-#define TELEMETRY_MARKER_DOWNLOAD_TIME   "DownloadTime_split"
-#define TELEMETRY_MARKER_DOWNLOAD_ERROR  "DownloadError_split"
-#define TELEMETRY_MARKER_INSTALL_TIME    "InstallTime_split"
-#define TELEMETRY_MARKER_INSTALL_ERROR   "InstallError_split"
-#define TELEMETRY_MARKER_UNINSTALL_TIME  "UninstallTime_split"
-#define TELEMETRY_MARKER_UNINSTALL_ERROR "UninstallError_split"
-#define TELEMETRY_MARKER_LAUNCH_TIME     "OverallLaunchTime_split"
-#define TELEMETRY_MARKER_LAUNCH_ERROR    "AppLaunchError_split"
-#define TELEMETRY_MARKER_CLOSE_TIME      "AppCloseTime_split"
-#define TELEMETRY_MARKER_CLOSE_ERROR     "AppCloseError_split"
-#define TELEMETRY_MARKER_SUSPEND_TIME    "SuspendTime_split"
-#define TELEMETRY_MARKER_RESUME_TIME     "ResumeTime_split"
-#define TELEMETRY_MARKER_HIBERNATE_TIME  "HibernateTime_split"
-#define TELEMETRY_MARKER_WAKE_TIME       "WakeTime_split"
-#define TELEMETRY_MARKER_APP_CRASHED     "AppCrashed_split"
+#define TELEMETRY_MARKER_PREFIX             "ENTS_INFO_RDKAM"
+
+#define TELEMETRY_MARKER_DOWNLOAD_TIME      TELEMETRY_MARKER_PREFIX "DownloadTime"
+#define TELEMETRY_MARKER_DOWNLOAD_ERROR     TELEMETRY_MARKER_PREFIX "DownloadError"
+#define TELEMETRY_MARKER_INSTALL_TIME       TELEMETRY_MARKER_PREFIX "InstallTime"
+#define TELEMETRY_MARKER_INSTALL_ERROR      TELEMETRY_MARKER_PREFIX "InstallError"
+#define TELEMETRY_MARKER_UNINSTALL_TIME     TELEMETRY_MARKER_PREFIX "UninstallTime"
+#define TELEMETRY_MARKER_UNINSTALL_ERROR    TELEMETRY_MARKER_PREFIX "UninstallError"
+#define TELEMETRY_MARKER_LAUNCH_TIME        TELEMETRY_MARKER_PREFIX "AppLaunchTime"
+#define TELEMETRY_MARKER_LAUNCH_ERROR       TELEMETRY_MARKER_PREFIX "AppLaunchError"
+#define TELEMETRY_MARKER_CLOSE_TIME         TELEMETRY_MARKER_PREFIX "AppCloseTime"
+#define TELEMETRY_MARKER_CLOSE_ERROR        TELEMETRY_MARKER_PREFIX "AppCloseError"
+#define TELEMETRY_MARKER_SUSPEND_TIME       TELEMETRY_MARKER_PREFIX "SuspendTime"
+#define TELEMETRY_MARKER_RESUME_TIME        TELEMETRY_MARKER_PREFIX "ResumeTime"
+#define TELEMETRY_MARKER_HIBERNATE_TIME     TELEMETRY_MARKER_PREFIX "HibernateTime"
+#define TELEMETRY_MARKER_WAKE_TIME          TELEMETRY_MARKER_PREFIX "WakeTime"
+#define TELEMETRY_MARKER_APP_CRASHED        TELEMETRY_MARKER_PREFIX "AppCrashed"
