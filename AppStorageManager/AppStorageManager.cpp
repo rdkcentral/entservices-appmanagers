e674a3495a3b9af67f42bd3db5767918331e2fbe
sajilal711 <shreyas.sajilal@gmail.com>
2026-03-30 17:54:39 +0530

    test

--- AppStorageManager/AppStorageManager.cpp
+++ AppStorageManager/AppStorageManager.cpp
@@ -132,6 +132,7 @@ namespace WPEFramework
         }
         if (nullptr != mStorageManagerImpl)
         {
+            do not merge
             Exchange::JAppStorageManager::Unregister(*this);
 
             // Stop processing:
