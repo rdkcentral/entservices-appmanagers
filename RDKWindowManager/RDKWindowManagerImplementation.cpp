5d10b57472b2ce027c500a04f5b91837a15c99db
db982 <db982@apac.comcast.com>
2026-04-15 21:32:20 +0530

    Resolving build errors

--- RDKWindowManager/RDKWindowManagerImplementation.cpp
+++ RDKWindowManager/RDKWindowManagerImplementation.cpp
@@ -252,8 +252,7 @@ Core::hresult RDKWindowManagerImplementation::Initialize(PluginHost::IShell* ser
                   request->mResult = CompositorController::createDisplay(request->mClient, request->mDisplayName, request->mDisplayWidth, request->mDisplayHeight, request->mVirtualDisplayEnabled, request->mVirtualWidth, request->mVirtualHeight, request->mTopmost, request->mFocus , request->mOwnerId, request->mGroupId);
                   time_t displayEndTime = RDKWindowManagerTelemetryReporting::getInstance().getCurrentTimestampMs();
                    const int duration = static_cast<int>(displayEndTime - displayStartTime);
-                LOGINFO("CompositorController::createDisplay, CreateDisplay timing: clientId:%s start_ms:%lld end_ms:%lld duration_ms:%d status:failed",
-                    clientId.c_str(),
+                LOGINFO("CompositorController::createDisplay, CreateDisplay timing: start_ms:%lld end_ms:%lld duration_ms:%d status:failed",
                     static_cast<long long>(displayStartTime),
                     static_cast<long long>(displayEndTime),
                     duration);
