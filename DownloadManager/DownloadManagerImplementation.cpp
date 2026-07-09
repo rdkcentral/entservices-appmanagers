ff2e939a3182753701f71c288152346b19b79e55
Saurav1604 <saurav.vinodh@gmail.com>
2026-07-09 09:54:24 +0530

    RDKEMW-15353: Added condition to skip pure timeout to remove spamming

--- DownloadManager/DownloadManagerImplementation.cpp
+++ DownloadManager/DownloadManagerImplementation.cpp
@@ -437,12 +437,14 @@ namespace Plugin {
             {
                 std::unique_lock<std::mutex> lock(mQueueMutex);
                 //Added wait_time to avoid spurious wakeups and to allow the thread to exit gracefully when mDownloaderRunFlag is set to false
-                mDownloadThreadCV.wait_for(lock,std::chrono::seconds(waitTime), [&] {
+                const bool ready = mDownloadThreadCV.wait_for(lock,std::chrono::seconds(waitTime), [&] {
                     return !mDownloaderRunFlag || !mPriorityDownloadQueue.empty() || !mRegularDownloadQueue.empty();
                 });
                 if (!mDownloaderRunFlag)
                     break;
-
+                if (!ready) {
+                    continue;
+                }
                 downloadRequest = pickDownloadJob();
             } //unlock mutex
 
