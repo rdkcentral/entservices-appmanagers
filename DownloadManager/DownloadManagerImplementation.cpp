6de906de5a3eb400ad782605443bea684fca0fa1
db982 <db982@apac.comcast.com>
2026-07-14 14:44:44 +0530

    Copilot comment addressing

--- DownloadManager/DownloadManagerImplementation.cpp
+++ DownloadManager/DownloadManagerImplementation.cpp
@@ -557,7 +557,16 @@ namespace Plugin {
                 std::lock_guard<std::mutex> lock(mQueueMutex);
                 mCurrentDownload.reset();
             }
-["fileLocator"] = locator;
+        }
+        LOGINFO("DM: Downloader thread exiting!");
+    }
+
+    void DownloadManagerImplementation::notifyDownloadStatus(const string& id, const string& locator, const DownloadReason reason)
+    {
+        JsonArray list = JsonArray();
+        JsonObject obj;
+        obj["downloadId"] = id;
+        obj["fileLocator"] = locator;
         if (reason != (DownloadReason)DOWNLOAD_REASON_NONE)
         {
             obj["failReason"] = getDownloadReason(reason);
