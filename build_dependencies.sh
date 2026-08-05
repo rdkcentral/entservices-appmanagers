b8cd89396feaa43e33cd56f4fe29745397252781
vrgoli <venkats.goli@gmail.com>
2026-08-05 09:44:26 +0000

    Thunder tools branch changed to R4_4-RDK

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -63,9 +63,9 @@ cd ..
 # Clone the required repositories
 
 
-git clone --branch  R4_4-RDK https://github.com/rdkcentral/ThunderTools.git
+git clone --branch  R4.4.3 https://github.com/rdkcentral/ThunderTools.git
 
-git clone --branch R4_4-RDK https://github.com/rdkcentral/Thunder.git
+git clone --branch R4.4.1 https://github.com/rdkcentral/Thunder.git
 
 git clone --branch topic/RDKEMW-19867 https://github.com/rdkcentral/entservices-apis.git
 
@@ -81,8 +81,7 @@ git clone -b develop https://github.com/rdkcentral/libPackage.git
 echo "======================================================================================"
 echo "building thunderTools"
 cd ThunderTools
-git checkout d5dd83c7c19c49c7f25c558c126500bd2d64f7a4
-#patch -p1 < $GITHUB_WORKSPACE/Tests/patches/00010-R4.4-Add-support-for-project-dir.patch
+patch -p1 < $GITHUB_WORKSPACE/Tests/patches/00010-R4.4-Add-support-for-project-dir.patch
 cd -
 
 
@@ -101,12 +100,11 @@ echo "==========================================================================
 echo "building thunder"
 
 cd Thunder
-git checkout 2c0fcc5529e7da734be558ca6efa05d934dcce31
-#patch -p1 < $GITHUB_WORKSPACE/Tests/patches/Use_Legact_Alt_Based_On_ThunderTools_R4.4.3.patch
-#patch -p1 < $GITHUB_WORKSPACE/Tests/patches/error_code_R4_4.patch
-#patch -p1 < $GITHUB_WORKSPACE/Tests/patches/1004-Add-support-for-project-dir.patch
-#patch -p1 < $GITHUB_WORKSPACE/Tests/patches/RDKEMW-733-Add-ENTOS-IDS.patch
-#patch -p1 < $GITHUB_WORKSPACE/Tests/patches/Jsonrpc_dynamic_error_handling.patch
+patch -p1 < $GITHUB_WORKSPACE/Tests/patches/Use_Legact_Alt_Based_On_ThunderTools_R4.4.3.patch
+patch -p1 < $GITHUB_WORKSPACE/Tests/patches/error_code_R4_4.patch
+patch -p1 < $GITHUB_WORKSPACE/Tests/patches/1004-Add-support-for-project-dir.patch
+patch -p1 < $GITHUB_WORKSPACE/Tests/patches/RDKEMW-733-Add-ENTOS-IDS.patch
+patch -p1 < $GITHUB_WORKSPACE/Tests/patches/Jsonrpc_dynamic_error_handling.patch
 cd -
 
 cmake -G Ninja -S Thunder -B build/Thunder \
