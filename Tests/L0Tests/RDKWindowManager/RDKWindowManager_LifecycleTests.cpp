7d30fcbba86e85aab3d3662d15da7c910f4d9afe
Venkateswar Rao Goli <42335144+vrgoli@users.noreply.github.com>
2026-08-06 13:37:15 +0530

    Update RDKWindowManager_LifecycleTests.cpp

--- Tests/L0Tests/RDKWindowManager/RDKWindowManager_LifecycleTests.cpp
+++ Tests/L0Tests/RDKWindowManager/RDKWindowManager_LifecycleTests.cpp
@@ -87,6 +87,10 @@ public:
     WPEFramework::Core::hresult GetFocused(std::string&) const override { return WPEFramework::Core::ERROR_NONE; }
     WPEFramework::Core::hresult GetScreenshot() override { return WPEFramework::Core::ERROR_NONE; }
     WPEFramework::Core::hresult SetAlias(const std::string&, const std::string&) override { return WPEFramework::Core::ERROR_NONE; }
+    WPEFramework::Core::hresult ShowSplashScreen(const bool) override { return WPEFramework::Core::ERROR_NONE; }
+    WPEFramework::Core::hresult SetBounds(const std::string&, const uint32_t, const uint32_t, const uint32_t, const uint32_t) override { return WPEFramework::Core::ERROR_NONE; }
+    WPEFramework::Core::hresult GetBounds(const std::string&, uint32_t&, uint32_t&, uint32_t&, uint32_t&) const override { return WPEFramework::Core::ERROR_NONE; }
+    WPEFramework::Core::hresult SetScale(const std::string&, const double, const double) override { return WPEFramework::Core::ERROR_NONE; }
     WPEFramework::Core::hresult GetScale(const std::string&, double&, double&) const override { return WPEFramework::Core::ERROR_NONE; }
 
     mutable std::atomic<uint32_t> _refCount;
