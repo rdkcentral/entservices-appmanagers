7ce9628341a2253ea5933bfacf35cb9a8d4f3861
Venkateswar Rao Goli <42335144+vrgoli@users.noreply.github.com>
2026-08-14 18:48:48 +0530

    Update PackageManager_ShellTests.cpp

--- Tests/L0Tests/PackageManager/PackageManager_ShellTests.cpp
+++ Tests/L0Tests/PackageManager/PackageManager_ShellTests.cpp
@@ -42,9 +42,9 @@ public:
     {
     }
 
-    void AddRef() const override
+    uint32_t AddRef() const override
     {
-        _refCount.fetch_add(1, std::memory_order_relaxed);
+        return _refCount.fetch_add(1, std::memory_order_relaxed) + 1;
     }
 
     uint32_t Release() const override
