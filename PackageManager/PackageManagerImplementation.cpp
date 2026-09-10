cf725dea9017ce64684e7b43be82de15e2a64912
Venkateswar Rao Goli <42335144+vrgoli@users.noreply.github.com>
2026-09-10 13:27:45 +0530

    Update PackageManagerImplementation.cpp - version is not mandatory for package metadata

--- PackageManager/PackageManagerImplementation.cpp
+++ PackageManager/PackageManagerImplementation.cpp
@@ -1402,7 +1402,7 @@ namespace Plugin {
         CHECK_CACHE()
         Core::hresult result = Core::ERROR_GENERAL;
 
-        if (packageId.empty() || version.empty())
+        if (packageId.empty())
         {
             return Core::ERROR_INVALID_PARAMETER;
         }
