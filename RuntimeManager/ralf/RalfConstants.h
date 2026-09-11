60e187c8eb5006e993ae6b2e14042bdec7bc4378
Arun Madhavan <arun_madhavan@comcast.com>
2026-09-11 09:21:45 -0400

    RDKEMW-22537: ralf - NWCongifuration clkeanup unused sections
    There will never be a DNSMASQ disabled configuration requirement
    since all apps are Firebolt and it should connect to Firebolt Endpoint
    for satisfying LifeCycle rules.

--- RuntimeManager/ralf/RalfConstants.h
+++ RuntimeManager/ralf/RalfConstants.h
@@ -35,11 +35,5 @@ namespace ralf
     const std::string RALF_HOST_TIMEZONE_DST_PATH = "/opt/persistent/timeZoneDST";
     const std::string RALF_TIMEZONE_PATH = "/etc/timezone";
     const std::string RALF_LOCALTIME_PATH = "/etc/localtime";
-#if 0 // Disabled for now, but will need when DNSMASQ is disabled in Dobby Network Configurations.
-    const std::string RALF_DEFAULT_RESOLV_CONF_FILE = "/etc/resolv.conf";
-    const std::string RALF_HOST_DEFAULT_RESOLV_CONF_FILE = "/etc/resolv.conf";
-    const std::string RALF_HOST_NOSTUB_NWMGR_RESOLV_CONF_FILE = "/run/NetworkManager/no-stub-resolv.conf";
-    const std::string RALF_HOST_NOSTUB_SYSTEMD_RESOLV_CONF_FILE = "/run/systemd/resolve/resolv.conf";
-#endif
     typedef std::pair<std::string, std::string> RalfPkgInfoPair; // <packageMetadataJsonPath, mountPoint>
 } // namespace ralf
