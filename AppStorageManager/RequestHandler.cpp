dcdad05b050bd9487c1ebfaebdeee45ced5c3843
madanagopalt <madanagopal123@gmail.com>
2026-09-24 14:30:39 +0530

    Update RequestHandler.cpp

--- AppStorageManager/RequestHandler.cpp
+++ AppStorageManager/RequestHandler.cpp
@@ -208,7 +208,6 @@ namespace WPEFramework
                     if (Core::ERROR_NONE != status)
                     {
                         LOGERR("appQuotaSizeProperty Failed for App Quota size retrivel, appId:%s!!!", appId.c_str());
-                        LOGINFO("quotaSize property not found for appId: %s, using default", appId.c_str());
                         storageInfo.quotaKB = 0;
                     }
 
